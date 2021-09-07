708e5a272b0637c968c17e9703777804964e61b1
Ezgi Sirip <ezgisirip@gmail.com>
2021-09-06 21:03:54 -0400

    update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/portfolio-website/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Ezgi Elouzeh',
   subHeading: 'Fullstack Engineer',
   // social
